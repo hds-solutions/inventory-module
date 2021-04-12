3569e5b1cb452fd54f3e6a1cf93a34e2c0b97444
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-04-12 16:05:00 -0400

    FIX: Pivot class is on same namespace Base\Pivot

--- app/Models/X_ProductLocator.php
+++ app/Models/X_ProductLocator.php
@@ -2,10 +2,9 @@
 
 namespace HDSSolutions\Finpar\Models;
 
-use App\Models\Base\Pivot;
 use HDSSolutions\Finpar\Traits\BelongsToCompany;
 
-abstract class X_ProductLocator extends Pivot {
+abstract class X_ProductLocator extends Base\Pivot {
     use BelongsToCompany;
 
     protected $table = 'locator_product';
